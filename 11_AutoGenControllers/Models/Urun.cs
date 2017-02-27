4777bce697f8b40eef32f8aa976cc757c0599043
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-27 15:45:51 +0300

    Auto-Generated Controller& Views

---
+++ 11_AutoGenControllers/Models/Urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _11_AutoGenControllers.Models
+{
+    public class Urun
+    {
+        public int UrunAd { get; set; }
+        public decimal UrunFiyat { get; set; }
+    }
+}
